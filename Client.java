4b1834b7d1b0263311dee30eb6652b0e0185a020
David Maiato <142182549+dmaiato@users.noreply.github.com>
2024-10-25 17:39:05 -0300

    Update Client.java

--- Client.java
+++ Client.java
@@ -1,17 +1,73 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class Client {
   // Atributos: nome, email, telefone.
   // Métodos: fazerReserva(), cancelarReserva().
 
   private String name;
-  private String email;
   private String phone;
+  private String email;
+  private List<Booking> bookings;
 
-  public void book() {
-    //sasasasas
+  /**
+   * Creates Client object.
+   * @param name Client name
+   * @param email Client email
+   * @param phone Client phone
+   */
+  public Client(String name, String email, String phone) {
+    this.name = name;
+    this.email = email;
+    this.phone = phone;
+    this.bookings = new ArrayList<>();
   }
 
-  public void cancelBooking() {
-    //oednojndwd
+  /**
+   * Returns name attribute.
+   * @return Client.name
+   */
+  public String getName() {
+    return this.name;
+  }
+
+  /**
+   * Returns phone attribute.
+   * @return Client.phone
+   */
+  public String getPhone() {
+    return this.phone;
+  }
+
+  /**
+   * Returns email attribute.
+   * @return
+   */
+  public String getEmail() {
+    return this.email;
+  }
+
+  /**
+   * Returns a Client's booked schedule.
+   * @return
+   */
+  public List<Booking> getBookings() {
+    return bookings;
+  }
+
+  /**
+   * Adds cross-refenced Booking object to attribute List.
+   * @param booking Booking object
+   */
+  public void book(Booking booking) {
+    this.bookings.add(booking);
+  }
+
+  /**
+   * Removes cross-refenced Booking object from attribute List.
+   * @param booking Booking object
+   */
+  public void cancelBooking(Booking booking) {
+    this.bookings.remove(booking);
   }
 }
