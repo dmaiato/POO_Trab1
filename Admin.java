17cd50b93db7c3db68c5cf1e7fad709b8ee666ea
David Maiato <142182549+dmaiato@users.noreply.github.com>
2024-10-25 17:37:28 -0300

    Update Admin.java

--- Admin.java
+++ Admin.java
@@ -1,14 +1,50 @@
+import java.util.List;
 
 public class Admin {
   
   private String name;
-  private int id;
+  private String id;
 
-  public void addHotel() {
-    //asasasa
+
+  /** System admin: can add and remove hotels from the system.*/
+  public Admin(String name, String id) {
+    this.name = name;
+    this.id = id;
   }
 
-  public void removeHotel() {
-    //asasasas
+  /**
+   * Returns name attribute.
+   * @return Admin.name
+   */
+  public String getName() {
+    return this.name;
+  }
+
+  /**
+   * Returns id attribute.
+   * @return Admin.id
+   */
+  public String getId() {
+    return this.id;
+  }
+
+  /**
+   * adds a new hotel to the booking system.
+   * @param name hotel name
+   * @param address hotel address
+   */
+  public Hotel addHotel(String name, String address) {
+    return new Hotel(name, address);
+  }
+
+  /**
+   * removes a hotel from the booking system.
+   * @param hotel hotel object to be removed
+   * @param hotelList list to have hotel removed from
+   * @return returns modified list
+   */
+  public List<Hotel> removeHotel(Hotel hotel, List<Hotel> hotelList) {
+    hotelList.remove(hotel);
+    return hotelList;
   }
 }
